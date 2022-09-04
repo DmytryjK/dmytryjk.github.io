c268dd3b1bf46cceaa118a685db54088f97661b7
Dmytryj Kyzyma <48493462+DmytryjK@users.noreply.github.com>
2022-09-04 18:47:37 +0300

    Delete script.js

--- FriendsApp/js/script.js
+++
@@ -1,7 +0,0 @@
-import filterCards from './modules/filterCards';
-import addActiveClass from './modules/addClassForNavElements';
-
-window.addEventListener('DOMContentLoaded', function() {
-    filterCards();
-    addActiveClass();
-});
